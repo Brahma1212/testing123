4bd38fdd2d10c043f12624357329358167f6b1e7
Brahma1212 <50369782+Brahma1212@users.noreply.github.com>
2019-05-21 20:22:39 +0530

    Update selenium.py

--- selenium.py
+++ selenium.py
@@ -3,14 +3,3 @@ from selenium import webdriver
 browser=webdriver.Chrome()
 browser.get('www.google.com')
 time.sleep(10)
-1
-2
-3
-4
-5
-6
-7
-8
-9aslkdjfaslfd
-sddlkfjsddklfjasddfkjasdf
-sddalfjasd
